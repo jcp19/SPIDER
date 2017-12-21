815073698583e32c163365705b11802b7fa55c9d
Joao Pereira <joaopereira.19@gmail.com>
2017-12-21 00:17:27 +0000

    fixed remove method

--- src/main/java/pt/minha/checker/events/EventIterator.java
+++ src/main/java/pt/minha/checker/events/EventIterator.java
@@ -7,26 +7,26 @@ package pt.minha.checker.events;
 import java.util.*;
 
 public class EventIterator implements Iterator<Event>{
-    private PriorityQueue<MyPair<Iterator<Event>, Event>> eventHeap;
+    private PriorityQueue<MyPair<ListIterator<Event>, Event>> eventHeap;
     //keeps track of the last Iterator next'd for the remove method
-    private Iterator<Event> lastIt;
+    private ListIterator<Event> lastIt;
 
-    public final Comparator<MyPair<Iterator<Event>,Event>> heapOrder = new Comparator<MyPair<Iterator<Event>, Event>>() {
+    public final Comparator<MyPair<ListIterator<Event>,Event>> heapOrder = new Comparator<MyPair<ListIterator<Event>, Event>>() {
         //doesnt support null arguments
-        public int compare(MyPair<Iterator<Event>, Event> o1, MyPair<Iterator<Event>, Event> o2) {
+        public int compare(MyPair<ListIterator<Event>, Event> o1, MyPair<ListIterator<Event>, Event> o2) {
             return o2.getSecond().getId() - o1.getSecond().getId();
         }
     };
 
     public EventIterator(Collection<List<Event>> eventLists) {
         //Holds the first value of a list and an iterator of the rest of the list
-        eventHeap = new PriorityQueue<MyPair<Iterator<Event>, Event>>(eventLists.size(), heapOrder);
+        eventHeap = new PriorityQueue<MyPair<ListIterator<Event>, Event>>(eventLists.size(), heapOrder);
 
         for(List<Event> eventList : eventLists) {
             if(!eventList.isEmpty()) {
-                Iterator<Event> it = eventList.iterator();
+                ListIterator<Event> it = eventList.listIterator();
                 Event firstElem = it.next();
-                eventHeap.add(new MyPair<Iterator<Event>, Event>(it, firstElem));
+                eventHeap.add(new MyPair<ListIterator<Event>, Event>(it, firstElem));
             }
         }
     }
@@ -39,15 +39,15 @@ public class EventIterator implements Iterator<Event>{
         if(!this.hasNext()) {
             throw new NoSuchElementException("There are no more elements");
         }
-        MyPair<Iterator<Event>, Event> heapTop = eventHeap.poll();
+        MyPair<ListIterator<Event>, Event> heapTop = eventHeap.poll();
 
-        Iterator<Event> it = heapTop.getFirst();
+        ListIterator<Event> it = heapTop.getFirst();
         Event next = heapTop.getSecond();
         lastIt = it;
 
         if(it.hasNext()) {
             Event toInsert = it.next();
-            eventHeap.add(new MyPair<Iterator<Event>, Event>(it, toInsert));
+            eventHeap.add(new MyPair<ListIterator<Event>, Event>(it, toInsert));
         }
         return next;
     }
@@ -56,7 +56,14 @@ public class EventIterator implements Iterator<Event>{
         if(lastIt == null) {
             throw new IllegalStateException();
         }
+        //puts the cursor before the to be next element
+        lastIt.previous();
+        //puts the cursor before the last returned element
+        lastIt.previous();
+        //removes the last returned element
         lastIt.remove();
+        //puts the cursor in its original position
+        lastIt.next();
         lastIt = null;
     }
 }
