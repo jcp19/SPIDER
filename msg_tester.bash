f252ef771e96a40ef1988a3c0a2e3a72be5a27c5
Joao Pereira <joaopereira.19@gmail.com>
2018-05-02 15:16:43 +0100

    updated msg_tester script

--- msg_tester.bash
+++ msg_tester.bash
@@ -1,7 +1,7 @@
 java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -r | tee /tmp/nre.log |
-grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc.txt
+grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc_msg.txt
 
-java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar | tee /tmp/re.log |grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc_msg.txt
+java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar | tee /tmp/re.log |grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc.txt
 
 diff mc_msg.txt mc.txt
 
