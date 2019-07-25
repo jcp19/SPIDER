57a73547bdc5b93f71c0b28c027defbd115aa01f
João Pereira <joaopereira.19@gmail.com>
2019-07-25 03:19:54 +0100

    Improve code style of the Stats class to be closer to the google java style guide.
    This class is particularly un-idiomatic.

--- src/main/java/pt/minha/checker/stats/Stats.java
+++ src/main/java/pt/minha/checker/stats/Stats.java
@@ -1,45 +1,56 @@
 package pt.minha.checker.stats;
 
-/**
- * Created by nunomachado on 11/05/17.
- */
+/** Created by nunomachado on 11/05/17. */
 public class Stats {
 
-    //general variables
-    public static long numEventsTrace = 0;
-    public static long numConstraints = 0;
-    public static long redundantEvents = 0;
-    public static long redundantSocketEvents = 0;
-    public static long prunedEvents = 0;
-    public static double buildingModeltime = 0;
+  // TODO: change static fields to instance variables
+  // TODO: change printStats to generate a String with the stats THAT WERE UPDATED instead of all
+  //       stats and instead of printing directly to the screen
 
-    //data race variables
-    public static long totalDataRaceCandidates = 0;
-    public static long totalDataRacePairs = 0;
-    public static double checkingTimeDataRace = 0;
+  // general variables
+  public static long numEventsTrace = 0;
+  public static long numConstraints = 0;
+  public static long redundantEvents = 0;
+  public static long redundantSocketEvents = 0;
+  public static long prunedEvents = 0;
+  public static double buildingModeltime = 0;
 
-    //message race variables
-    public static long totalMsgRaceCandidates = 0;
-    public static long totalMsgRacePairs = 0;
-    public static double checkingTimeMsgRace = 0;
+  // data race variables
+  public static long totalDataRaceCandidates = 0;
+  public static long totalDataRacePairs = 0;
+  public static double checkingTimeDataRace = 0;
 
-    public static void printStats(){
-        System.out.println("\n=======================");
-        System.out.println("        RESULTS        ");
-        System.out.println("=======================");
-        System.out.println("> Number of events in trace:\t\t\t"+numEventsTrace);
-        System.out.println("> Number of redundant events in trace:\t\t"+redundantEvents);
-        System.out.println("> Number of redundant socket events in trace:\t"+redundantSocketEvents);
-        System.out.println("> Number of pruned events in trace:\t\t"+prunedEvents);
-        System.out.println("> Number of constraints in model:\t\t"+numConstraints);
-        System.out.println("> Time to generate constraint model:\t\t"+(buildingModeltime/(double)1000)+" seconds");
-        System.out.println("\n## DATA RACES:");
-        System.out.println("  > Number of data race candidates:\t\t"+ totalDataRaceCandidates);
-        System.out.println("  > Number of actual data races:\t\t"+totalDataRacePairs);
-        System.out.println("  > Time to check all candidates:\t\t"+(checkingTimeDataRace /(double)1000)+" seconds");
-        System.out.println("\n## MESSAGE RACES:");
-        System.out.println("  > Number of message race candidates:\t\t"+ totalMsgRaceCandidates);
-        System.out.println("  > Number of actual message races:\t\t"+totalMsgRacePairs);
-        System.out.println("  > Time to check all candidates:\t\t"+(checkingTimeMsgRace /(double)1000)+" seconds");
-    }
+  // message race variables
+  public static long totalMsgRaceCandidates = 0;
+  public static long totalMsgRacePairs = 0;
+  public static double checkingTimeMsgRace = 0;
+
+  public static void printStats() {
+    System.out.println("\n=======================");
+    System.out.println("        RESULTS        ");
+    System.out.println("=======================");
+    System.out.println("> Number of events in trace:\t\t\t" + numEventsTrace);
+    System.out.println("> Number of redundant events in trace:\t\t" + redundantEvents);
+    System.out.println("> Number of redundant socket events in trace:\t" + redundantSocketEvents);
+    System.out.println("> Number of pruned events in trace:\t\t" + prunedEvents);
+    System.out.println("> Number of constraints in model:\t\t" + numConstraints);
+    System.out.println(
+        "> Time to generate constraint model:\t\t"
+            + (buildingModeltime / (double) 1000)
+            + " seconds");
+    System.out.println("\n## DATA RACES:");
+    System.out.println("  > Number of data race candidates:\t\t" + totalDataRaceCandidates);
+    System.out.println("  > Number of actual data races:\t\t" + totalDataRacePairs);
+    System.out.println(
+        "  > Time to check all candidates:\t\t"
+            + (checkingTimeDataRace / (double) 1000)
+            + " seconds");
+    System.out.println("\n## MESSAGE RACES:");
+    System.out.println("  > Number of message race candidates:\t\t" + totalMsgRaceCandidates);
+    System.out.println("  > Number of actual message races:\t\t" + totalMsgRacePairs);
+    System.out.println(
+        "  > Time to check all candidates:\t\t"
+            + (checkingTimeMsgRace / (double) 1000)
+            + " seconds");
+  }
 }
