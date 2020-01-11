0b9c25279f61813f4f3e5c31e1e3a6825cadadf4
João Pereira <joaopereira.19@gmail.com>
2020-01-11 20:38:29 +0000

    Remove outdated test scripts

--- msg_tester.bash
+++
@@ -1,8 +0,0 @@
-java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -r | tee /tmp/nre.log |
-grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc_msg.txt
-
-java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar | tee /tmp/re.log |grep "^~~ (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc.txt
-
-diff mc_msg.txt mc.txt
-
-#rm mc_rex.txt mc.txt
--- tester.bash
+++
@@ -1,8 +0,0 @@
-java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -r | tee /tmp/nre.log |
-grep "^-- (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc.txt
-
-java -jar ./target/minha-checker-1.0-SNAPSHOT-jar-with-dependencies.jar | tee /tmp/re.log |grep "^-- (" | sed -e 's/.*@\(.*\),.*@\(.*\))/(\1,\2)/' |  sort | uniq > mc_rex.txt
-
-diff mc_rex.txt mc.txt
-
-#rm mc_rex.txt mc.txt
