2b753458bf8846346f713364fc9e39952d72894d
João Pereira <joaopereira.19@gmail.com>
2020-01-14 00:21:14 +0000

    Remove comments in portuguese

--- evaluation/measure-data-races.bash
+++ evaluation/measure-data-races.bash
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-# imprime header csv
-# para todos os scripts nas pastas que quero:
-#   - corre jar data races sem redundancia | usa o resultado no gawk para imprimir meia linha do csv (sem newline no final)
-#   - corre jar data races com redundancia | usa o resultado no gawk para imprimir meia linha do csv (com newline no final)
 CSV_OUTPUT_FILE='data_race_results.csv'
 CSV_HEADER='Test Case, #events in the trace, #contraints, #data race candidates, #data race candidate locations, #actual data races, #actual data race locations, time to check candidates, #removed events (RW + IT) (Redundant RW %), #contraints(R), #data race candidates (R), #data race candidate locations(R), #actual data races(R), #actual data race locations(R), time to check candidates(R)'
-#FOLDERS_WITH_TRACES=('../traces/micro-benchmarks' '../traces/cyclon-spider')
-FOLDERS_WITH_TRACES=('../traces/micro-benchmarks')
-TIMEOUT='120s'
+FOLDERS_WITH_TRACES=('../traces/micro-benchmarks' '../traces/cyclon-spider')
+TIMEOUT='30s'
 
 YELLOW='\033[1;33m'
 NO_COLOR='\033[0m'
